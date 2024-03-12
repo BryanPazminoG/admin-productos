81f4a990f31d4b8569a09957aa8552bc9b2e6e40
BryanPazminoG <bpbryalex98@gmail.com>
2024-03-11 19:27:02 -0500

    Creacion proyecto

---
+++ src/main/java/ec/edu/espe/tercerparcial/examenpazmino/ExamenpazminoApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.tercerparcial.examenpazmino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenpazminoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenpazminoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/edu/espe/tercerparcial/examenpazmino/ExamenpazminoApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.tercerparcial.examenpazmino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenpazminoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
